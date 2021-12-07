a29795aa4b30495b2a3d9b5f852dd802b64ff458
Kim Nguyen <kimnivore@gmail.com>
2021-12-07 11:39:20 -0800

    completed App imports and set up forlikePost func

--- src/App.js
+++ src/App.js
@@ -9,21 +9,31 @@ import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
-import './components/Posts/LikeSection.js';
-import './components/Posts/Post.js';
-import './components/Posts/PostHeader.js';
-import './components/Posts/Posts.css';
-import './components/Posts/Posts.js';
-import './components/SearchBar/SearchBar.css';
-import './components/SearchBar/SearchBar.js';
-import './dummy-data.js';
+import LikeSection from './components/Posts/LikeSection.js';
+import Post from './components/Posts/Post.js';
+import Postheader from './components/Posts/PostHeader.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data.js';
+
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  
+  const [posts, setPosts] = useState(dummyData);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const likePost = postId => {
+    const moreLikes = posts.map(post => {
+      if(post.id === postId) {
+        return { ...post, likes: setPosts(post + 1)}
+      } else {
+        return post;
+      }
+    })
+    setPosts(moreLikes);
+  
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -46,3 +56,8 @@ const App = () => {
 };
 
 export default App;
+
+
+// import './components/Posts/Posts.css';
+// import './components/Posts/Posts.js';
+// import './components/SearchBar/SearchBar.css';
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
